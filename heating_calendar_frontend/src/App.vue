4db10823bd54d9b19c9a74b0e54f5af6830ec1e3
heidenbloed <69988488+heidenbloed@users.noreply.github.com>
2024-03-18 23:44:47 +0100

    Add calendar component.

--- heating_calendar_frontend/src/App.vue
+++ heating_calendar_frontend/src/App.vue
@@ -1,3 +1,6 @@
 <template>
-  <div class="text-xl text-center">Heating calendar</div>
+  <div class="flex flex-col place-items-center gap-4 p-4">
+    <h1 class="font-bold text-4xl">Heating calendar</h1>
+    <VCalendar transparent borderless :disabled-dates="[{'start': null, 'end': new Date()}]"/>
+  </div>
 </template>
