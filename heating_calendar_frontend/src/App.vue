fbea60484e515db5b68d7a270895ef0e67b7c19d
heidenbloed <69988488+heidenbloed@users.noreply.github.com>
2024-03-19 22:09:20 +0100

    Add highlighting of dates in calendar.

--- heating_calendar_frontend/src/App.vue
+++ heating_calendar_frontend/src/App.vue
@@ -1,6 +1,65 @@
 <template>
-  <div class="flex flex-col place-items-center gap-4 p-4">
-    <h1 class="font-bold text-4xl">Heating calendar</h1>
-    <VCalendar transparent borderless :disabled-dates="[{'start': null, 'end': new Date()}]"/>
+  <div class="mx-auto w-96">
+    <div class="flex flex-col place-items-center gap-4 p-4">
+      <h1 class="text-4xl font-bold">Heating calendar</h1>
+      <VCalendar
+        transparent
+        borderless
+        expanded
+        :rows="2"
+        :first-day-of-week="2"
+        :disabled-dates="[{ start: null, end: new Date() }]"
+        :attributes="calendarAttributes"
+        @dayclick="onDayClick"
+      />
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { CalendarDay } from "v-calendar/dist/types/src/utils/page.js";
+import { computed, ref } from "vue";
+
+interface CalendarDate {
+  day: number;
+  month: number;
+  year: number;
+}
+
+const highlightDates = ref<Array<CalendarDate>>([]);
+const calendarAttributes = computed(() => {
+  const dates = highlightDates.value.map((calanderDate) => {
+    return new Date(
+      calanderDate.year,
+      calanderDate.month - 1,
+      calanderDate.day,
+    );
+  });
+  return [
+    {
+      highlight: "red",
+      dates: dates,
+      order: 0,
+    },
+  ];
+});
+
+function onDayClick(calandarDay: CalendarDay) {
+  const calendarDate = {
+    day: calandarDay.day,
+    month: calandarDay.month,
+    year: calandarDay.year,
+  };
+  const highlightIndex = highlightDates.value.findIndex(
+    (elem) =>
+      elem.day === calendarDate.day &&
+      elem.month === calendarDate.month &&
+      elem.year === calendarDate.year,
+  );
+  if (highlightIndex >= 0) {
+    highlightDates.value.splice(highlightIndex, 1);
+  } else {
+    highlightDates.value.push(calandarDay);
+  }
+}
+</script>
