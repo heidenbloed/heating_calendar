4ec748954ebb4704fe4778142df95122ea217346
Tim <69988488+heidenbloed@users.noreply.github.com>
2024-03-29 21:52:24 +0000

    Fix nginx config and install scripts.

--- build_and_start_serving.sh
+++ build_and_start_serving.sh
@@ -6,12 +6,14 @@ cd ..
 
 echo "Run vite build."
 cd heating_calendar_frontend
-npm run build -- --outDir /var/www/heating_calendar_frontend
+npm install
+npx vite build --outDir dist
+sudo cp -r dist /var/www/heating_calendar_frontend
 cd ..
 
 echo "Update Nginx config and restart."
-cp heating_calendar.nginx.conf /etc/nginx/sites-available/
+sudo cp heating_calendar.nginx.conf /etc/nginx/sites-available/
 if [ ! -f "/etc/nginx/sites-enabled/heating_calendar.nginx.conf" ]; then
-ln -s "/etc/nginx/sites-available/heating_calendar.nginx.conf" "/etc/nginx/sites-enabled/"
+sudo ln -s "/etc/nginx/sites-available/heating_calendar.nginx.conf" "/etc/nginx/sites-enabled/"
 fi
-systemctl restart nginx
+sudo systemctl restart nginx
--- heating_calendar_db/install_service.sh
+++ heating_calendar_db/install_service.sh
@@ -1,12 +1,12 @@
 echo "Stop service"
-systemctl stop heating_calendar_db.service
+sudo systemctl stop heating_calendar_db.service
 echo "Copy service"
-cp heating_calendar_db.service /etc/systemd/system/
+sudo cp heating_calendar_db.service /etc/systemd/system/
 echo "Enable service"
-systemctl enable heating_calendar_db.service
+sudo systemctl enable heating_calendar_db.service
 echo "Start service"
-systemctl start heating_calendar_db.service
+sudo systemctl start heating_calendar_db.service
 echo "Status of service"
-systemctl status heating_calendar_db.service
+sudo systemctl status heating_calendar_db.service
 echo "Set up table"
 . ./init_db.sh
