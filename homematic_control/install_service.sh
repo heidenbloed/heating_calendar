1876962dbf3bc4ee58c10328d0d736293969171a
heidenbloed <69988488+heidenbloed@users.noreply.github.com>
2025-12-08 22:18:55 +0100

    Update to latest homematic api.

--- homematic_control/install_service.sh
+++ homematic_control/install_service.sh
@@ -1,10 +1,5 @@
 echo "Stop service"
 sudo systemctl stop homematic_control.service
-echo "Create venv and install python requirements"
-python3 -m pip install virtualenv
-python3 -m virtualenv venv
-source venv/bin/activate
-pip install -r requirements.txt
 echo "Copy service"
 sudo cp homematic_control.service /etc/systemd/system/
 echo "Enable service"
--- homematic_control/lab_temp_controller.py
+++ homematic_control/lab_temp_controller.py
@@ -1,20 +1,28 @@
 import homematicip
 import homematicip.group
 import homematicip.home
+import asyncio
 
-LAB_HEATING_GROUP_ID = "f97c443b-75f9-430a-9458-d24be5c3ec05"
+LAB_HEATING_GROUP_ID = "24b8872c-0ff7-49d5-86f0-f9f783285fd1"
 
 
 class LabTempController:
     def __init__(self):
         home_config = homematicip.find_and_load_config_file()
         self._home = homematicip.home.Home()
-        self._home.set_auth_token(home_config.auth_token)
-        self._home.init(home_config.access_point)
+        asyncio.run(
+            self._home.init_async(
+                access_point_id=home_config.access_point,
+                auth_token=home_config.auth_token,
+            )
+        )
 
     def set_temp(self, temp: float):
-        self._home.get_current_state()
+        asyncio.run(self.set_temp_async(temp))
+
+    async def set_temp_async(self, temp: float):
+        await self._home.get_current_state_async()
         lab_heating: homematicip.group.HeatingGroup = self._home.search_group_by_id(
             LAB_HEATING_GROUP_ID
         )
-        lab_heating.set_point_temperature(temp)
+        await lab_heating.set_point_temperature_async(temp)
