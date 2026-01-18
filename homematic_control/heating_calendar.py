1fa67942413bae2f3e73e84d77066bcc758d37ea
heidenbloed <69988488+heidenbloed@users.noreply.github.com>
2026-01-18 21:51:00 +0100

    Create heating calendar backend.

--- homematic_control/heating_calendar.py
+++ homematic_control/heating_calendar.py
@@ -1,53 +1,23 @@
 import os
+import sqlite3
 
-import requests
-
-DB_URL_BASE = os.environ["HEATING_CALENDAR_DB_URL"]
-DB_USER = os.environ["HEATING_CALENDAR_DB_USER"]
-DB_PASS = os.environ["HEATING_CALENDAR_DB_PASS"]
-DB_URL_SIGNIN = f"{DB_URL_BASE}/signin"
-DB_URL_QUERY = f"{DB_URL_BASE}/sql"
-DB_NAMESPACE = "heating_calendar"
-DB_NAME = "heating_calendar"
+DB_PATH = os.environ["HEATING_CALENDAR_DB_PATH"]
 TODAYS_HEATING_SETTING_QUERY = (
-    "SELECT * FROM heating_calendar WHERE date > time::now() - 12h && date <"
-    " time::now() + 12h;"
+    "SELECT * FROM heating_calendar WHERE date BETWEEN date('now', 'start of day')"
+    " AND date('now', 'start of day', '+1 day' ,'-1 second');"
 )
 
 
-def _sign_in_db() -> str:
-    sign_in_response = requests.post(
-        url=DB_URL_SIGNIN,
-        json={"user": DB_USER, "pass": DB_PASS},
-        headers={"Accept": "application/json"},
-    )
-    sign_in_response.raise_for_status()
-    token = sign_in_response.json()["token"]
-    return token
-
-
-def _query_db_for_todays_heating_setting(token: str) -> list:
-    query_response = requests.post(
-        url=DB_URL_QUERY,
-        headers={
-            "Authorization": f"Bearer {token}",
-            "Accept": "application/json",
-            "surreal-ns": DB_NAMESPACE,
-            "surreal-db": DB_NAME,
-        },
-        data=TODAYS_HEATING_SETTING_QUERY,
-    )
-    query_response.raise_for_status()
-    return query_response.json()
+def _query_db_for_todays_heating_setting(cursor: sqlite3.Cursor) -> list:
+    return cursor.execute(TODAYS_HEATING_SETTING_QUERY).fetchall()
 
 
 def is_today_heating_on() -> bool:
-    db_token = _sign_in_db()
-    todays_heating_settings = _query_db_for_todays_heating_setting(token=db_token)
-    return (
-        len(todays_heating_settings) > 0
-        and len(todays_heating_settings[0]["result"]) > 0
-    )
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    todays_heating_settings = _query_db_for_todays_heating_setting(cursor=cursor)
+    conn.close()
+    return len(todays_heating_settings) > 0
 
 
 if __name__ == "__main__":
